bebcc93b39f91364757d5894b6d393a38037d178
Diwash-Niroula <dniroula10@gmail.com>
2024-10-18 03:04:52 -0400

    Initialize background color on page load

--- lab-2/js/page-color-changer.js
+++ lab-2/js/page-color-changer.js
@@ -25,3 +25,5 @@ function updateBackgroundColor() {
 redSlider.addEventListener('input', updateBackgroundColor);
 greenSlider.addEventListener('input', updateBackgroundColor);
 blueSlider.addEventListener('input', updateBackgroundColor);
+// Step 4: Initialize background color on page load
+updateBackgroundColor();
